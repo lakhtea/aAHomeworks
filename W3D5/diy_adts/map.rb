7968adcff517c32a5648abc62bcefe2164c25fdd
Lakhte <lakhteagha@gmail.com>
2021-01-15 01:03:28 -0500

    Data structure classes

---
+++ W3D5/diy_adts/map.rb
@@ -0,0 +1,21 @@
+class Map
+    def initialize
+        @map = []
+    end
+
+    def set(key, value)
+        @map.include?(key) ? @map.each { |pair| pair[1] = value if pair[0] == key } : @map << [key, value]
+    end
+
+    def get(key)
+        @map.each { |pair| puts pair[1] if pair[0] == key }
+    end
+
+    def delete(key)
+        @map.each_with_index { |pair, idx| @map.delete_at(idx) if pair[0] == key }
+    end
+
+    def show
+        p @map
+    end
+end
